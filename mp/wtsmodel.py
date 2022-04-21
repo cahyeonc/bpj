d036c2572918f8fbca9f7ac96a988bf9568cb449
cahyeonc <cahyeonc@naver.com>
2022-04-21 12:11:36 +0900

    파이프라인 수정

--- mp/wtsmodel.py
+++ mp/wtsmodel.py
@@ -2,25 +2,6 @@ import numpy as np
 import json
 from tensorflow import keras
 #from pyjosa.josa import Josa # 조사 오픈소스
-
-reconstructed_model = keras.models.load_model("dataset/WTS_model.h5")
-
-dict_file = "dataset/sentence.json"
-word_list = "dataset/wordlist.json"
-dic = json.load(open(dict_file,"r"))
-words = json.load(open(word_list,"r"))   
-
-chars = sorted(list(set(dic.keys()))) 
-char_indices = dict((c, i) for i, c in enumerate(chars)) # 문자 → ID
-indices_char = dict((i, c) for i, c in enumerate(chars)) # ID → 문자
-
-maxlen = 5
-sentences = []
-next_chars = []
-
-for i in range(len(words)-1):
-    sentences.append(words[i])
-    next_chars.append(words[i + 1])
     
 def sample(preds, temperature=1.0):
     preds = np.asarray(preds).astype('float64')
@@ -31,20 +12,24 @@ def sample(preds, temperature=1.0):
     return np.argmax(probas)     
 
 def predict_mo(lst):
+    reconstructed_model = keras.models.load_model("WTS_model.h5")
+    char_indices = json.load(open("char_indices.json","r"))
+    indices_char = json.load(open("indices_char.json","r"))   
+    
     result =''
     
     for i in lst:
-        
         if i[-1] == '요' or i[-1] == '다':
             result += i + ' '
             continue
             
-        x = np.zeros((1, 5, len(chars)))
+        x = np.zeros((1, 5, len(char_indices)))
         x[0, len(i), char_indices[i]] = 1.
 
         preds = reconstructed_model.predict(x, verbose=0)[0]
         next_index = sample(preds)
-        next_char = indices_char[next_index]
+        next_char = indices_char[str(next_index)]
+        
         
         #y = josa_fuc(i, next_char)
         result += (i + next_char + ' ')
